2d55adbd6559549a00abdb71f8bb84c1e93dc885
john <jmockler94@gmail.com>
2021-01-12 16:23:18 +0100

    first commit

---
+++ EigenWrapperUWP/EigenWrapperUWP.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <Eigen/Dense>
+
+#define EXPORT_API __declspec(dllexport)
+
+using namespace Eigen;
+extern "C" {
+
+
+	EXPORT_API void calculateLeastSquares(float* H, float* X) {
+		//calculates determinant of H, then finds X = U*V_transpose and calculates determinant
+		typedef Map<MatrixXf> MapMatrix;
+		MapMatrix H_map(H, 3, 3);
+
+		MapMatrix xMap(X, 3, 3);
+
+		JacobiSVD<MatrixXf> svd(H_map, ComputeFullU | ComputeFullV);
+
+		xMap = svd.matrixV() * svd.matrixU().transpose();
+
+		//float det = X_map.determinant();
+		//return X;
+	}
+
+	EXPORT_API void calculateTransform(float* model_centroid, float* detected_centroid, float* R, float* H) {
+		typedef Map<MatrixXf> MapMatrix;
+
+		MapMatrix model_map(model_centroid, 3, 1);
+		MapMatrix detected_map(detected_centroid, 3, 1);
+
+		MapMatrix R_map(R, 3, 3);
+
+		MatrixXf T = detected_map - R_map * model_map;
+
+		//float* H = new float(16);
+		MapMatrix H_map(H, 4, 4);
+
+		H_map = MatrixXf::Identity(4, 4);
+		H_map.block(0, 0, 3, 3) = R_map;
+		H_map.block(0, 3, 3, 1) = T;
+		//return H;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HololensPointRegistration
  a unity plugin for 3D-3D point registration
+
+Isometric case is based on method in the paper Least-squares fitting of two 3-D point sets. IEEE T Pattern Anal 
+by Arun et. al.
+
+Anisometric case is adapted from matlab code in Iterative Solution for Rigid-Body Point-Based Registration with
+Anisotropic Weighting by Balachadran et. al.
+
+Current work in progress
