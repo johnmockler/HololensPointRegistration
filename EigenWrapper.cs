6aebd6acf5509f6edad9246c5f3bc06fa304f900
john <jmockler94@gmail.com>
2021-01-15 12:15:33 +0100

    uploaded c# wrapper

---
+++ EigenWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+
+public class EigenWrapper
+{
+    [DllImport("EigenWrapper", CallingConvention = CallingConvention.Cdecl)]
+    public static extern float registerIsotropic([MarshalAs(UnmanagedType.LPArray)] float[] X, [MarshalAs(UnmanagedType.LPArray)] float[] Y, int N, 
+        [MarshalAs(UnmanagedType.LPArray)] float[] outR, [MarshalAs(UnmanagedType.LPArray)] float[] outT);
+
+    [DllImport("EigenWrapper", CallingConvention = CallingConvention.Cdecl)]
+    public static extern float registerAnisotropic([MarshalAs(UnmanagedType.LPArray)] float[] X, [MarshalAs(UnmanagedType.LPArray)] float[] Y, [MarshalAs(UnmanagedType.LPArray)] float[] W, int N, float threshold,
+    [MarshalAs(UnmanagedType.LPArray)] float[] outR, [MarshalAs(UnmanagedType.LPArray)] float[] outT);
+
+}
